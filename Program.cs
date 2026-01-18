e058b1a856d89de9132406192b60ccb99c0e178e
Chase Petrowsky <chasepetrowsky@gmail.com>
2026-01-18 11:49:27 -0700

    Added entity framework core and setup configuration for SQl server connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
+using LastCallMotorAuctions.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
