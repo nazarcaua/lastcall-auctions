061892024131f34638f124d5eb0e5c6274507646
Chase Petrowsky <chasepetrowsky@gmail.com>
2026-01-19 13:18:43 -0700

    Add API controller routing support (AddController and MapControllers)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using LastCallMotorAuctions.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add services to the container.
+builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -13,5 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
