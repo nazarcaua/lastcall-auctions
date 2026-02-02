a907e746157ebfd6a04a75a36205a4fb6c2c5a11
Chase Petrowsky <chasepetrowsky@gmail.com>
2026-02-01 17:27:31 -0700

    Added CreateListingDto and ListingResponseDto with validation

---
+++ DTOs/CreateListingDto.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LastCallMotorAuctions.API.DTOs
+{
+    public class CreateListingDto
+    {
+        [Required]
+        [StringLength(120, MinimumLength = 1)]
+        public string Title { get; set; } = null;
+
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(1886, 2100)]
+        public short Year { get; set; }
+
+        [Required]
+        public int MakeId { get; set; }
+
+        [Required]
+        public int ModelId { get; set; }
+
+        [StringLength(17)]
+        public string? Vin { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int? Mileage { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public byte ConditionGrade { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Country { get; set; } = null;
+
+        [StringLength(20)]
+        public string? PostalCode { get; set; }
+    }
+}
