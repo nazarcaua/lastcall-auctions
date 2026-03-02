db1d495203c4205a4fad1750f447ee707e5a4696
Chase Petrowsky <chasepetrowsky@gmail.com>
2026-03-02 12:49:17 -0700

    Initial DTO for the Admin Dashboard Summary

---
+++ DTOs/AdminDashboardSummary.cs
@@ -0,0 +1,9 @@
+﻿namespace LastCallMotorAuctions.API.DTOs
+{
+    public class AdminDashboardSummary
+    {
+        public int PendingSellerRequests { get; set; }
+        public int PendingListings { get; set; }
+        public int LiveAuctions { get; set; }
+    }
+}
