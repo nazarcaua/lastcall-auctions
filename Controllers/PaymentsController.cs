6ad4cfdf97c6cb9a3996fce09879ca0bda941fa8
Chase Petrowsky <chasepetrowsky@gmail.com>
2026-03-09 13:12:28 -0600

    Created PaymentsController and method for getting buyer id

---
+++ Controllers/PaymentsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using LastCallMotorAuctions.API.DTOs;
+using LastCallMotorAuctions.API.Services;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace LastCallMotorAuctions.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Policy = "BuyerOnly")]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+        private readonly ILogger<PaymentsController> _logger;
+
+        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
+        {
+            _paymentService = paymentService;
+            _logger = logger;
+        }
+
+        private int GetBuyerId()
+        {
+            var idClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(idClaim))
+            {
+                throw new UnauthorizedAccessException("User id not found in token.");
+            }
+
+            return int.Parse(idClaim);
+        }
+
+    }
+}
